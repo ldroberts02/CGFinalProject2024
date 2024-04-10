8e30ea9e086a0340f80dab134338efcfc6700c2d
EricWRogers <ericwilliamrogers@gmail.com>
2024-04-10 11:58:00 -0500

    update camera

--- src/Canis/Camera.hpp
+++ src/Canis/Camera.hpp
@@ -14,7 +14,7 @@ namespace Canis
     };
 
     // Default camera values
-    const float YAW = 0.0f;
+    const float YAW = 90.0f;
     const float PITCH = 0.0f;
     const float SPEED = 50.0f;
     const float SENSITIVITY = 0.1f;
